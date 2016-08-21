29d86c31febc8d29ce2f4eb016169c9eee5225c4
hikari1426 <hikaridev@outlook.com>
2016-08-21 19:34:59 -0300

    Texture ID Class.
    Only with a getter.

---
+++ src/me/hikari1426/textures/ModelTexture.java
@@ -0,0 +1,14 @@
+package me.hikari1426.textures;
+
+public class ModelTexture {
+	private int textureID;
+	
+	public ModelTexture(int id) {
+		this.textureID = id;
+	}
+	
+	public int getID()
+	{
+		return this.textureID;
+	}
+}
